2c58cd0ebee99952ac18e12ec6f97830eb66f320
rajeshdegala530 <rajeshdegala@gmail.com>
2020-07-23 12:45:37 +0100

    Changed PageObjects from .root to /src

--- src/Features/step-definitions/steps/Bannertest.js
+++ src/Features/step-definitions/steps/Bannertest.js
@@ -1,6 +1,6 @@
 import {Then} from 'cucumber'
 import URL from '../../../../Pages/URL'
-import HomePage from '../../../../Pages/HomePage'
+import HomePage from '../../../PagesObjects/HomePage'
 
 Then('banner is displayed on top of the page',()=>{
 
